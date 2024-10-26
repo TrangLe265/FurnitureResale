1d1a997eef854158c0170b49e1951b1e8a985ec4
TrangLe265 <trang.le@myy.haaga-helia.fi>
2024-10-26 14:49:34 +0300

    Added photo-adding fucntions

---
+++ hooks/ImagePicker.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react';
+import { View, Image, Platform, StyleSheet, Text } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Ionicons from '@expo/vector-icons/Ionicons'; 
+import { Pressable } from 'react-native';
+
+
+import { Button, SmlButton } from '../styling/Components';
+import { colors } from '../styling/colors';
+
+const ImagePickerScreen = () => {
+    const [image, setImage] = useState(null); 
+
+
+    useEffect(() => {
+        (async () => {
+            //so that the code only executed on phone platform
+            if (Platform.OS !== 'web'){
+                //geting permission to access media lib
+                const libraryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync(); 
+                if (libraryStatus.status !== 'granted'){
+                    alert('We need library permission to make this function work!')
+                }
+
+                //getting permission to access camera
+                const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+                if (cameraStatus.status !== 'granted') {
+                    alert('We need camera permission to make this function work!')
+                }
+            }
+        }) (); 
+    },[]); 
+
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4,3],
+            quality:   1,
+        }); 
+
+        if (!result.canceled){
+            setImage(result.assets[0].uri); 
+            console.log(result); 
+        }
+    }; 
+
+    return (
+        <View style={styles.container}>
+            <SmlButton
+                onPress={pickImage}
+            >
+                <Text>Choose image</Text>
+            </SmlButton>
+            {/*If photo is successfully retrieved then display it*/}
+            {image ? (
+                <Image 
+                source = {{uri:image}}
+                style={styles.image} 
+            />
+
+            ) : (
+                <Ionicons 
+                    name="image"
+                    size= {200}
+                    color = {colors.white}
+                />
+            )}
+            
+            
+        </View>
+
+    );
+}
+
+export default ImagePickerScreen; 
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10, 
+      height: 300,
+    },
+    image: {
+      width: 200,
+      height: 200,
+      marginTop: 20,
+    },
+  });
+
--- tabs/NewAd.js
+++ tabs/NewAd.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View,  TouchableOpacity } from 'react-native'; 
+import React, { useState, useEffect} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, ScrollView} from 'react-native'; 
+import { Pressable } from 'react-native';
+
+import ImagePickerScreen from '../hooks/ImagePicker';
 //import icons for nav
 import Ionicons from '@expo/vector-icons/Ionicons'; 
 
@@ -15,94 +18,90 @@ export default function NewAdScreen(){
         <KeyboardAvoidingView 
             style={styles.container}
             behavior='padding'
-        > 
-            <Card>
-                <T.h1>Create a new annoucement</T.h1>
-                <HorizontalDivider/>
-                <HorizontalSpacing/>
-                <Row>
-                    <T.bodyText>Name:</T.bodyText>
-                    <Input  
-                        editable
-                        numberOfLines={2}
-                        //placeholder = 'What are you selling'
-                        //value={title}
-                    />
-                </Row>
-                <Row>
-                    <T.bodyText>Brand: </T.bodyText>
-                    <Input  
-                        editable
-                        numberOfLines={2}
-                        //placeholder='Do you know the manufacturer?'
-                        //value={title}
-                    />
-                </Row>
-                <Row>
-                    <T.bodyText>Price (€): </T.bodyText>
-                    <Input  
-                        editable
-                        
+        >
+            <ScrollView>
+                <Card>
+                    <T.h1>Create a new annoucement</T.h1>
+                    <HorizontalDivider/>
+                    <HorizontalSpacing/>
+                    <Row>
+                        <T.bodyText>Name:</T.bodyText>
+                        <Input  
+                            editable
+                            numberOfLines={2}
+                            //placeholder = 'What are you selling'
+                            //value={title}
+                        />
+                    </Row>
+                    <Row>
+                        <T.bodyText>Brand: </T.bodyText>
+                        <Input  
+                            editable
+                            numberOfLines={2}
                             //placeholder='Do you know the manufacturer?'
                             //value={title}
-                    />
-                </Row>
-
-                <HorizontalDivider/>
-                <HorizontalSpacing/>
-                <View>
-                    <T.bodyText>Product category: </T.bodyText>
-                    <Row>
-                        <SmlButton>
-                            <Text>Sofa</Text>   
-                        </SmlButton>
-                        <SmlButton>
-                            <Text>Storage</Text>   
-                        </SmlButton>
-                        <SmlButton>
-                            <Text>Tables</Text>   
-                        </SmlButton>
-                        <SmlButton>
-                            <Text>Beds</Text>   
-                        </SmlButton>
-                        <SmlButton>
-                            <Text>Chairs & Armcharis</Text>   
-                        </SmlButton>
+                        />
                     </Row>
-                </View>
-                <HorizontalDivider/>
-                <HorizontalSpacing/>
-                
-                <View>
-                    <T.bodyText>Description: </T.bodyText>
-                    <Input  
-                        editable
-                        multiline
-                        numberOfLines={5}
-                        style={{width: '100%', height: 100}}
-                        //value={title}
-                    />
-                </View>
+                    <Row>
+                        <T.bodyText>Price (€): </T.bodyText>
+                        <Input  
+                            editable
+                            
+                                //placeholder='Do you know the manufacturer?'
+                                //value={title}
+                        />
+                    </Row>
 
-                <HorizontalDivider/>
-                <HorizontalSpacing/>
-                <Row>
-                    <T.bodyText>Pictures of the product:</T.bodyText>
-                    <TouchableOpacity>
-                        <Ionicons name={'camera'} color={colors.purple} size={35}/>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                        <Ionicons name={'image'} color={colors.purple} size={35}/>
-                    </TouchableOpacity>
-                
-                </Row>
-                
-        
+                    <HorizontalDivider/>
+                    <HorizontalSpacing/>
+                    <View>
+                        <T.bodyText>Product category: </T.bodyText>
+                        <Row>
+                            <SmlButton>
+                                <Text>Sofa</Text>   
+                            </SmlButton>
+                            <SmlButton>
+                                <Text>Storage</Text>   
+                            </SmlButton>
+                            <SmlButton>
+                                <Text>Tables</Text>   
+                            </SmlButton>
+                            <SmlButton>
+                                <Text>Beds</Text>   
+                            </SmlButton>
+                            <SmlButton>
+                                <Text>Chairs & Armcharis</Text>   
+                            </SmlButton>
+                        </Row>
+                    </View>
+                    <HorizontalDivider/>
+                    <HorizontalSpacing/>
+                    
+                    <View>
+                        <T.bodyText>Description: </T.bodyText>
+                        <Input  
+                            editable
+                            multiline
+                            numberOfLines={5}
+                            style={{width: '100%', height: 100}}
+                            //value={title}
+                        />
+                    </View>
+                    <HorizontalSpacing/>
+                    <HorizontalDivider/>
+                    
+                    <ImagePickerScreen />
 
-            </Card>
-            <Button>
-                <Text>Add product</Text>   
-            </Button>
+                    <Button>
+                        <Text>Add product</Text>   
+                    </Button>
+                </Card>
+            
+                    
+                
+                
+            </ScrollView>
+            
         </KeyboardAvoidingView>
     ); 
 }
@@ -114,5 +113,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
       },
+      scrollView: {
+        flexGrow: 1,
+        alignItems: 'center'
+      }
    
 })
